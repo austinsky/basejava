c16aae250ae8aa51ba5aaad2a2168ba3e9b579d1
lex <lex@mapha.org>
2023-12-21 17:40:48 +0300

    Lesson 1. Домашнее задание

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,56 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private static final int CAPACITY_STORAGE = 10000;
+
+    Resume[] storage = new Resume[CAPACITY_STORAGE];
+
+    private int size = 0;
 
     void clear() {
+        size = 0;
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        if (size < CAPACITY_STORAGE) {
+            storage[size++] = r;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = storage[i];
+        }
+        return result;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
